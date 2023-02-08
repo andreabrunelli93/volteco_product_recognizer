ce176af2b4837fb439532deb377a1155a97e9c26
Andrea Brunelli <andrea.brunelli@24consulting.it>
2023-02-08 14:27:41 +0100

    test pulsanti

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ import numpy as np
 
 import requests
 import webbrowser
-import cv2
-import skimage.measure
-from skimage.transform import rescale, resize, downscale_local_mean
-
 
 
 st.set_page_config(page_title="OCR with Streamlit", page_icon=":camera:", layout="wide")
@@ -24,15 +20,17 @@ st.title("VOLTECO Product Recognizer")
 
 def easy_ocr_process(img):
 
-    reader = easyocr.Reader(['it','en']) # this needs to run only once to load the model into memory
+    reader = easyocr.Reader(['en']) # this needs to run only once to load the model into memory
 
     image = Image.open(img)
 
     height, width = image.size
     if width > 2000:
         image = image.resize((height//4 ,width//4), Image.ANTIALIAS)
+        st.write("Compressione forte")
     if width > 1000:
         image = image.resize((height//2 ,width//2), Image.ANTIALIAS)
+        st.write("Compressione ridotta")
  
     testo = reader.readtext(np.asarray(image), detail = 0, paragraph=True)
     testo_clean = ' '.join(testo)
@@ -43,27 +41,27 @@ def easy_ocr_process(img):
     keyword_extracted = rake_nltk_var.get_ranked_phrases()
     return keyword_extracted[0]
 
-
+#Scarico la sitemap dei prodotti
 df_product = pd.read_xml('https://volteco.com/product-sitemap.xml')
 list_of_product = df_product['loc'].apply( lambda x: x.rsplit('/', 1)[-1])
 df_product['name'] = list_of_product
-#df_product
 
-
+#Inizializzo la chiamata al modello per similarità
 API_TOKEN = "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"
-
 API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
+#Chiama l'api per la similarità
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
+#Indice e probabiltà dei top3 per similarità
 def find_top_3_indices(numbers):
     sorted_numbers = sorted(enumerate(numbers), key=lambda x: x[1], reverse=True)
     return [i[0] for i in sorted_numbers[:3]], [i[1] for i in sorted_numbers[:3]]
     
-
+#Upload della foto
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
@@ -79,7 +77,7 @@ if uploaded_file is not None:
     for i in range(3):
         st.write(f"Ehi ehi, potresti avere davanti: **{df_product.iloc[best_three[i]]['name']}**, con una probabilità del **{probability[i]*100:.2f}%**")
         if st.button(f"Vai al prodotto {i+1}: {df_product.iloc[best_three[i]]['name']}"):
-            webbrowser.open_new_tab(df_product.iloc[best_three[i]]['loc'])
+            webbrowser.open(df_product.iloc[best_three[i]]['loc'])
        
 
 
